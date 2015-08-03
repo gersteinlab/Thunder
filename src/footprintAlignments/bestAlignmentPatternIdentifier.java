0dbca8a69d18de3b77059bc98383ee2443ca3fe3
jdmcpeek <david.mcpeek@yale.edu>
2015-08-03 03:41:17 -0400

    add a removeCrossGeneReads function, which strips out reads that could belong to many genes

--- src/footprintAlignments/bestAlignmentPatternIdentifier.java
+++ src/footprintAlignments/bestAlignmentPatternIdentifier.java
@@ -13,6 +13,9 @@ import java.util.Iterator;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -27,7 +30,6 @@ public class bestAlignmentPatternIdentifier {
         this.transcriptToGeneMap = new gtfParser(gencodeDataFile).getTranscriptGenes();
     }
     
-    
     public Map<String, ArrayList<String>> getReadTranscriptMap() throws IOException {
         Map<String, ArrayList<String>> readToTranscriptMap; 
         readToTranscriptMap = new HashMap<String, ArrayList<String>>();
@@ -35,15 +37,38 @@ public class bestAlignmentPatternIdentifier {
         for (final SAMRecord samRecord : inputBam) {
             String transcriptID = samRecord.getReferenceName();
             String readName = samRecord.getReadName();
-            if (readToTranscriptMap.containsKey(transcriptID)) {
-                readToTranscriptMap.get(transcriptID).add(readName);
+            if (readToTranscriptMap.containsKey(readName)) {
+                readToTranscriptMap.get(readName).add(transcriptID);
             } else {
-                readToTranscriptMap.put(transcriptID, new ArrayList<String>(Arrays.asList(readName)));
+                readToTranscriptMap.put(readName, new ArrayList<String>(Arrays.asList(transcriptID)));
             }            
         }
+        // if a read matches transcripts that live on different genes, just remove it from the map, we don't need it.        
+        removeCrossGeneReads(readToTranscriptMap); 
         return readToTranscriptMap;
     }
     
+    // when are we supposed to remove a read? If there is any non-uniformity in genes that the transcripts belong to? I feel like that totally winnows down the set.
+    public void removeCrossGeneReads(Map<String, ArrayList<String>> readToTranscriptMap) {
+        Iterator it = readToTranscriptMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            ArrayList<String> transcripts = (ArrayList<String>) pair.getValue();
+            
+            Set<String> uniqueGenes = new HashSet<String>();
+            
+            for (int i = 0; i < transcripts.size(); i++) {
+                String nextGene = transcriptToGeneMap.get(transcripts.get(i));
+                uniqueGenes.add(nextGene);
+            }
+            
+            if (uniqueGenes.size() > 1) {
+                it.remove();
+            }
+            
+        }
+        
+    }
     
     
     public static void main(String[] args) throws IOException { 
@@ -52,7 +77,21 @@ public class bestAlignmentPatternIdentifier {
         bestAlignmentPatternIdentifier test;
         test = new bestAlignmentPatternIdentifier(new File("./data/fullFootPrintAlignment.sorted.bam"), new File("./data/gencode.v21.transcripts.gtf.txt"));
         
-        test.getReadTranscriptMap();
+        Map<String, ArrayList<String>> readTranscriptMap = test.getReadTranscriptMap(); 
+        
+        
+        Iterator it = readTranscriptMap.entrySet().iterator();
+        
+        BufferedWriter testAgain;
+        testAgain = new BufferedWriter(new FileWriter("./data/testAgain.txt"));
+        
+        while(it.hasNext()){
+            Map.Entry pair = (Map.Entry)it.next();
+            testAgain.write(pair.getKey() + "  -  " + pair.getValue()); 
+            testAgain.newLine();
+        }
+        
+        
     }
     
 }
